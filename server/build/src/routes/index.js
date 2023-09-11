e44d3b6ff0582a80c46958c264425f3482d161da
psamirt <p_samir@hotmail.com>
2023-09-10 19:33:21 -0500

    backend terminado

--- server/build/src/routes/index.js
+++ server/build/src/routes/index.js
@@ -12,35 +12,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const router = (0, express_1.Router)();
 const nodemailer = require('nodemailer');
-const { google } = require('googleapis');
 require('dotenv').config();
-const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REFRESH_TOKEN } = process.env;
+const { EMAIL, PASS, TO_EMAIL } = process.env;
 router.post('/send', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { name, email, message } = req.body;
-    const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, REDIRECT_URI);
-    oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
     try {
-        const accessToken = yield oAuth2Client.getAccessToken();
         const transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: 'smtp.office365.com',
+            port: 587,
+            secure: false,
             auth: {
-                type: 'OAuth2',
-                user: 'contacto.paolotello@gmail.com',
-                clientId: CLIENT_ID,
-                clientSecret: CLIENT_SECRET,
-                refresh_token: REFRESH_TOKEN,
-                accessToken: accessToken
+                user: EMAIL,
+                pass: PASS
             }
         });
-        const mailOptions = {
-            from: 'contacto.paolotello@gmail.com',
-            to: 'p_samir@hotmail.com',
+        const result = yield transporter.sendMail({
+            from: EMAIL,
+            to: TO_EMAIL,
             subject: 'Portafolio contactado',
             text: `Nombre: ${name}\nCorreo Electrónico: ${email}\nMensaje: ${message}`
-            // Puedes personalizar el cuerpo del mensaje aquí con los datos del formulario.
-        };
-        const result = yield transporter.sendMail(mailOptions);
-        res.status(200).send('enviado');
+        });
+        console.log(result);
+        res.status(200).json({ ok: true, message: 'Mensaje enviado con éxito' });
     }
     catch (error) {
         console.log(error);
